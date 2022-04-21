deda6c4c86c8969a0c58a3d16040b2c4241ff8bc
Jie He <77890601+JieHE-2020@users.noreply.github.com>
2022-04-21 17:14:44 +0200

    Delete Archive directory

--- Archive/material/artifact/complete_track/dataset_generation/translation/level_2/TP_atom/original_TP_atom/until/simplest/simplest_until_atom_eventually/simplest_until_atom_eventually_organizer.py
+++
@@ -1,7 +0,0 @@
-from translation.level_2.TP_atom.original_TP_atom.eventually.simplest.simplest_eventually_atom_organizer \
-    import SimplestEventuallyAtomOrganizer
-
-
-class SimplestUntilAtomEventuallyOrganizer(SimplestEventuallyAtomOrganizer):
-
-    pass
--- Archive/material/artifact/fast_track/dataset_generation/translation/level_2/TP_atom/not_rise_TP_atom/public_class/not_rise_tp_atom_process/not_rise_tp_atom_organizer.py
+++
@@ -1,6 +0,0 @@
-from translation.level_2.TP_atom.rise_TP_atom.public_class.rise_tp_atom_process.\
-    rise_tp_atom_organizer import RiseTPAtomOrganizer
-
-
-class NotRiseTPAtomOrganizer(RiseTPAtomOrganizer):
-    pass
